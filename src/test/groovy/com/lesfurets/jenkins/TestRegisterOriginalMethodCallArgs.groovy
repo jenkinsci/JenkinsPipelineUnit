2abdac54fb56d951817fb6f5d5f5976e8b5a80f2
Brian Ray <brianeray@users.noreply.github.com>
2020-03-30 15:08:36 -0700

    Support not cloning method call arguments
    v1.3 introduced a guard against call stack arguments mutating away from
    the values they had at the time of the original method call. The guard
    is to clone the argument instances at the time of the call.
    
    In most use cases this ideal. But for arguments whose types are not
    `Cloneable` the guard turns the arg values into `String` instances.
    This is good fallback behavior but can break tests that do
    fine-grained validations against nested structures, for example
    `UnmodifiableMap` instances.
    
    In those use cases the pre-v1.3 behavior of preserving the original
    references may be desirable. The new
    `PipelineTestHelper.cloneArgsOnMethodCallRegistration` property can
    be set by the test to disable the cloning.

---
+++ src/test/groovy/com/lesfurets/jenkins/TestRegisterOriginalMethodCallArgs.groovy
@@ -0,0 +1,60 @@
+package com.lesfurets.jenkins
+
+import org.junit.Before
+import org.junit.Test
+
+import com.lesfurets.jenkins.unit.BasePipelineTest
+
+import static org.junit.Assert.*
+
+class TestRegisterOriginalMethodCallArgs extends BasePipelineTest {
+
+    @Override
+    @Before
+    void setUp() throws Exception {
+        scriptRoots += "src/test/jenkins"
+        super.setUp()
+    }
+
+    @Test
+    void should_not_always_clone_args() {
+        helper.cloneArgsOnMethodCallRegistration = false
+
+        runScript("job/immutableMapArgs.jenkins")
+
+        def arg = helper.callStack.find { call ->
+            call.methodName == "writeFile"
+        }.args.first()
+
+        //Ensure that the arg is the original uncloned binding variable, and 
+        //that we can inspect it in detail using all the normal Map interfaces.
+        assertTrue(arg.is(binding.pretendArgsFromFarUpstream))
+
+        assertEquals(arg.getClass().simpleName, "UnmodifiableMap")
+        assertEquals(arg.size(), 2)
+
+        assertEquals(arg.file, "foo.txt")
+        assertEquals(arg.text, "All bar, all the time")
+    }
+
+    @Test
+    void should_usually_clone_args() {
+        //By default the helper clones args on registering calls.
+
+        runScript("job/immutableMapArgs.jenkins")
+
+        def arg = helper.callStack.find { call ->
+            call.methodName == "writeFile"
+        }.args.first()
+
+        //Ensure that the arg is not the original binding variable, and not 
+        //even the same type, because that variable was an uncloneable 
+        //UnmodifiableMap. The cloning logic turns uncloneables into Strings.
+        assertFalse(arg.is(binding.pretendArgsFromFarUpstream))
+
+        assertEquals(arg.getClass().simpleName, "String")
+
+        assertTrue(arg.contains("file=foo.txt"))
+        assertTrue(arg.contains("text=All bar, all the time"))
+    }
+}
