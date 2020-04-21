5c90975bbf6f92f92344f6ab70a6cb8e2743e7ab
Stanislav Ovchar <stanislav.ovchar@nordigy.ru>
2020-04-21 11:40:40 +0300

    Try to preload lib classes

--- src/main/groovy/com/lesfurets/jenkins/unit/InterceptingGCL.groovy
+++ src/main/groovy/com/lesfurets/jenkins/unit/InterceptingGCL.groovy
@@ -35,39 +35,41 @@ class InterceptingGCL extends GroovyClassLoader {
     @Override
     Class parseClass(GroovyCodeSource codeSource, boolean shouldCacheSource)
             throws CompilationFailedException {
+        println "Parsing class: ${codeSource.getName()}"
         Class clazz = super.parseClass(codeSource, shouldCacheSource)
         interceptClassMethods(clazz.metaClass, helper)
         return clazz
     }
 
-    // @Override
-    // Class<?> loadClass(String name) throws ClassNotFoundException {
-    //     // Source from: groovy-all-2.4.6-sources.jar!/groovy/lang/GroovyClassLoader.java:710
-    //     Class cls = null
-    //     // try groovy file
-    //     try {
-    //         URL source = resourceLoader.loadGroovySource(name);
-    //         // if recompilation fails, we want cls==null
-    //         cls = recompile(source, name, null);
-    //     } catch (IOException ioe) {
-    //     } finally {
-    //         if (cls == null) {
-    //             removeClassCacheEntry(name);
-    //         } else {
-    //             setClassCacheEntry(cls);
-    //         }
-    //     }
+    @Override
+    Class<?> loadClass(String name) throws ClassNotFoundException {
+        // Source from: groovy-all-2.4.6-sources.jar!/groovy/lang/GroovyClassLoader.java:710
+        println "Loading class: $name"
+        Class cls = null
+        // try groovy file
+        try {
+            URL source = resourceLoader.loadGroovySource(name);
+            // if recompilation fails, we want cls==null
+            cls = recompile(source, name, null);
+        } catch (IOException ioe) {
+        } finally {
+            if (cls == null) {
+                removeClassCacheEntry(name);
+            } else {
+                setClassCacheEntry(cls);
+            }
+        }
 
-    //     if (cls == null) {
-    //         // no class found, using parent's method
-    //         return super.loadClass(name)
-    //     }
+        if (cls == null) {
+            // no class found, using parent's method
+            cls = super.loadClass(name)
+        }
 
-    //     // Copy from this.parseClass(GroovyCodeSource, boolean)
-    //     cls.metaClass.invokeMethod = helper.getMethodInterceptor()
-    //     cls.metaClass.static.invokeMethod = helper.getMethodInterceptor()
-    //     cls.metaClass.methodMissing = helper.getMethodMissingInterceptor()
+        // Copy from this.parseClass(GroovyCodeSource, boolean)
+        cls.metaClass.invokeMethod = helper.getMethodInterceptor()
+        cls.metaClass.static.invokeMethod = helper.getMethodInterceptor()
+        cls.metaClass.methodMissing = helper.getMethodMissingInterceptor()
 
-    //     return cls;
-    // }
+        return cls;
+    }
 }
