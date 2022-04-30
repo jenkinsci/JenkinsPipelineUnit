2b249fed1b805702cd5e1f17fd942028b1d33b5e
Nestor Acuna-Blanco <nacuna85@gmail.com>
2022-04-30 16:41:38 +0200

    Adds test case changing the binding value.

--- src/test/groovy/com/lesfurets/jenkins/TestSharedLibraryAccessibleParams.groovy
+++ src/test/groovy/com/lesfurets/jenkins/TestSharedLibraryAccessibleParams.groovy
@@ -6,12 +6,15 @@ import org.junit.Test
 
 import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
 import static com.lesfurets.jenkins.unit.global.lib.ProjectSource.projectSource
+import static org.assertj.core.api.Assertions.assertThat
 
 class TestSharedLibraryAccessibleParams extends DeclarativePipelineTest {
 
     private final String JOB_NAME = "params_not_accessible"
     private final String JOB_PATH = "job/library/${JOB_NAME}.jenkins"
     private final String LIB_DIR = this.class.getResource("/libs/$JOB_NAME").getFile()
+    private final String BINDING_VAR = "testVar"
+    private final String BINDING_VAL = "notBroken"
 
     @Override
     @Before
@@ -30,13 +33,23 @@ class TestSharedLibraryAccessibleParams extends DeclarativePipelineTest {
 
     @Test
     void accessible_params_test() {
-        binding.setVariable('testVar', 'notBroken')
-        runScript(JOB_PATH)
-        assertJobStatusSuccess()
+        run_test_with_bindings {assertJobStatusSuccess()}
+    }
+
+    @Test
+    void change_binding_test() {
+        run_test_with_bindings {assertThat(binding.getVariable(BINDING_VAR)).isNotEqualTo(BINDING_VAL)}
     }
 
     @Test(expected = MissingPropertyException.class)
     void not_accessible_params_test() {
         runScript(JOB_PATH)
     }
+
+    private void run_test_with_bindings(Closure assertion) {
+        binding.setVariable(BINDING_VAR, BINDING_VAL)
+        runScript(JOB_PATH)
+        assertion()
+    }
+
 }
