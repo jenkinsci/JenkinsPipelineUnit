969ac00d65a62726b5c1f39ca431d423ceb2dad7
stchar <head.crab.meat@gmail.com>
2024-07-10 14:37:08 +0200

    cleanup: tests

--- src/test/groovy/com/lesfurets/jenkins/unit/CallStackDumpTest.groovy
+++ src/test/groovy/com/lesfurets/jenkins/unit/CallStackDumpTest.groovy
@@ -37,16 +37,12 @@ class CallStackDumpTest extends BasePipelineTest {
         assertCallStackContains('callStackDump.someMethod1()')
         assertCallStack().doesNotContain('callStackDump.someMethod2()')
 
+        // Do not clear call stack continue your test
+
         // Call method2 and expect callstack to match
         script.someMethod2()
         assertCallStackContains('callStackDump.someMethod1()')
         assertCallStackContains('callStackDump.someMethod2()')
 
-        // Reset using clearCallStack call
-        clearCallStack()
-        // Call method1 and expect callstack to match
-        script.someMethod1()
-        assertCallStackContains('callStackDump.someMethod1()')
-        assertCallStack().doesNotContain('callStackDump.someMethod2()')
     }
 }
